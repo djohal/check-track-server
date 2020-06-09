1637f754f3bc4b2ded63bc9d7fed2c1e6fc11248
Dilpreet Johal <dilpreet.johal022@gmail.com>
2020-06-09 19:34:38 -0400

    feat(mongoose): integrate with mongoose

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const mongoUri =
+  'mongodb+srv://dj-admin:dj-pass@cluster0-9i7ir.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.log('Error connecting to Mongo ', err);
+});
 
 app.get('/', (req, res) => {
   res.send('hello');
