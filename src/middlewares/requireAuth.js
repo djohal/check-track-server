a23c0461e51e8c0a73f01c07fedfb53aedd876d2
Dilpreet Johal <dilpreet.johal022@gmail.com>
2020-06-09 22:10:14 -0400

    feat: add requireAuth middleware

---
+++ src/middlewares/requireAuth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = mongoose.model('User');
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+  // authorization === 'Bearer ewrdsdsafsdafasd'
+
+  if (!authorization) {
+    return res.status(401).send('You must be logged in.');
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  jwt.verify(token, 'DJ_TRACK_APP', async (err, payload) => {
+    if (err) {
+      return res.status(401).send('You must be logged in.');
+    }
+
+    const { userId } = payload;
+
+    const user = await User.findById(userId);
+    req.user = user;
+    next();
+  });
+};
