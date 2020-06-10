999a307eae0bed73652de6dffd490421c86e1bde
Dilpreet Johal <dilpreet.johal022@gmail.com>
2020-06-09 22:58:57 -0400

    feat: signin route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -20,4 +20,25 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(422).send('Please provide an email and a password');
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(401).send('Invalid email or password');
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, 'DJ_TRACK_APP');
+    res.send({ token });
+  } catch (error) {
+    return res.status(401).send('Invalid email or password');
+  }
+});
+
 module.exports = router;
