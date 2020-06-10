3d5b3cbab6df9cb3b7ac629578fd029ebca80394
Dilpreet Johal <dilpreet.johal022@gmail.com>
2020-06-10 08:27:53 -0400

    feat(track): create new track

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -14,4 +14,20 @@ router.get('/tracks', async (req, res) => {
   res.send(tracks);
 });
 
+router.post('/track', async (req, res) => {
+  const { name, locations } = req.body;
+
+  if (!name || !locations) {
+    res.status(422).send('You must provide a name and locations');
+  }
+
+  try {
+    const track = new Track({ name, locations, userId: req.user._id });
+    await track.save();
+    res.send(track);
+  } catch (error) {
+    res.status(422).send({ error: error.message });
+  }
+});
+
 module.exports = router;
