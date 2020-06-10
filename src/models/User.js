ad53ecc0a9920fcccc959affad52a271b51cddda
Dilpreet Johal <dilpreet.johal022@gmail.com>
2020-06-09 22:36:12 -0400

    feat: used bcrypt to encrypt pass

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -12,5 +13,45 @@ const userSchema = new mongoose.Schema({
     minlength: 4,
   },
 });
+
+userSchema.pre('save', function (next) {
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) {
+        return next(err);
+      }
+
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (loginPassword) {
+  const user = this;
+
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(loginPassword, user.password, (err, isMatch) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatch) {
+        return reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
 
 mongoose.model('User', userSchema);
